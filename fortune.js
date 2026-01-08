db22a3db1823d9672c68fba8b8a7864a82289c52
Reochan88 <suekireososukereo@gmail.com>
2026-01-08 13:34:48 +0900

    Create fortune.js

---
+++ fortune.js
@@ -0,0 +1,12 @@
+function fortune() {
+  const list = [
+    "🌟 大吉：最高の1日になる",
+    "😊 中吉：良い流れ",
+    "🙂 小吉：平穏",
+    "😐 末吉：様子見",
+    "⚠️ 凶：無理しないで"
+  ];
+
+  const r = Math.floor(Math.random() * list.length);
+  document.getElementById("result").textContent = list[r];
+}
